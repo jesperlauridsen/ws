2a7baf47ca45b90407dc2f9d1ad2d37fc6b66094
Jesper Lauridsen <jesper@lauridsen.org>
2025-10-20 08:52:01 +0200

    yay

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@ import { RouterView } from 'vue-router';
         <nav>
           <a href="#hero">Home</a>
           <a href="#who">Who Am I</a>
-          <span>< / ></span>
+          <div class="logo-container">
+            <span class="logo"><span class="logo-line"></span></span>
+          </div>
           <a href="#work">Work</a>
           <a href="#contact">Contact</a>
         </nav>
@@ -35,6 +37,52 @@ nav {
   text-align: center;
   margin-top: 2rem;
   text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+}
+
+.logo-container {
+  display: flex;
+  width: 80px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.logo-line {
+  width: 2px;
+  height: 24px;
+  background-color: white;
+  display: inline-block;
+  position: relative;
+  transform: rotate(45deg);
+  top: -2.5px;
+}
+
+.logo::after {
+  content: '';
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  border-left: 2px solid white;
+  border-top: 2px solid white;
+  transform: rotate(-45deg);
+  margin-left: -22px;
+  margin-top: 3px;
+}
+
+.logo::before {
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-left: 2px solid white;
+  border-top: 2px solid white;
+  transform: rotate(135deg);
+  position: absolute;
+  margin-left: 10px;
+  margin-top: 3px;
 }
 
 nav a {
